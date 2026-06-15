75dff3a266dff1b7c775c71e867188dc3d824209
Pubert <pubert@Puberts-MacBook-Pro.local>
2026-06-15 13:47:10 +0200

    Just finshed the vggish and clap implmentations

--- TUNI_emotion_data_extraction/tuni_extraction_config.py
+++ TUNI_emotion_data_extraction/tuni_extraction_config.py
@@ -8,7 +8,7 @@ DATA_OUTPUT_DIR = (
     "/Users/pubert/Downloads/SkyNote/OTHER_DATASETS/TUNI_emotion_dataset/embeddings/tuni_embeddings")
 
 tuni_emotion_extraction = ExtractionConfig(
-    extractors=["opensmile", "clap", "whisper"], #add extractor params?
+    extractors=["opensmile", "clap", "whisper", "vggish"], #add extractor params? also re-add clap and vggish?
     output_dir=DATA_OUTPUT_DIR,
     window_lengths=[3.0, 0.5],  # Extract for both 3s and 0.5s frames
     batch_size="auto",
